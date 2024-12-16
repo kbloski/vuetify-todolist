96e18e1e89f6092b0cc7a43dd706a681017aa0f6
Kamil Błoński <kblonski02@gmail.com>
2024-12-16 15:14:54 +0100

    Learn app-bar

--- vuetify-project/src/components/layout/TheNavigation.vue
+++ vuetify-project/src/components/layout/TheNavigation.vue
@@ -1,8 +1,16 @@
 <template>
-    <v-app-bar color="grey-lighten-2"></v-app-bar>
-    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+    <v-app-bar 
+        scroll-behavior="hide collapse elevate fade-image"
+        scroll-threshold="20"
+        color="grey-lighten-2" 
+        image="/logo.png"
+    >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer color="grey-darken-2" ></v-navigation-drawer>
 </template>
 
-<style lang="scss" scoped>
+<script>
 
-</style>
+</script>
