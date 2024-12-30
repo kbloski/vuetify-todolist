cef0bc87f68429504220b319adb377dafe321278
Kamil Błoński <kblonski02@gmail.com>
2024-12-30 05:06:55 +0100

    Add layout to project

--- vue-todolist/src/App.vue
+++ vue-todolist/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <v-app>
-    <v-main>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+
+    <v-app-bar
+      color="grey-lighten-2"
+      title="Application bar"
+      flat
+    >
+      <template v-slot:append>
+        <v-switch
+          v-model="order"
+          false-value="0"
+          label="Toggle order"
+          true-value="-1"
+          hide-details
+          inset
+        ></v-switch>
+      </template>
+    </v-app-bar>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
       <router-view />
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
 
 <script lang="ts" setup>
-  //
+ 
 </script>
